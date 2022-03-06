c7b7c7ecc617ee72864b588f7510b3b34411f93f
Ines <inespontes110@gmail.com>
2022-03-06 18:58:38 +0000

    Create function to get result

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const resultDisplay = document.getElementById(`result`);
 
 const possibleChoices = document.querySelectorAll("button");
 
-let userChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 
@@ -25,4 +26,27 @@ function generateComputerChoice() {
   if (randomNumber === 3) {
     computerChoice = "Paper";
   }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "It's a draw 🤷‍♀️";
+  }
+  if (
+    (computerChoice === "Rock" && userChoice === "Scissors") ||
+    (computerChoice === "Scissors" && userChoice === "Paper") ||
+    (computerChoice === "Paper" && userChoice === "Rock")
+  ) {
+    result = "You Lost 😒";
+  }
+  if (
+    (computerChoice === "Rock" && userChoice === "Paper") ||
+    (computerChoice === "Paper" && userChoice === "Scissors") ||
+    (computerChoice === "Scissors" && userChoice === "Rock")
+  ) {
+    result = "You Win! 🥳";
+  }
+
+  resultDisplay.innerHTML = result;
 }
