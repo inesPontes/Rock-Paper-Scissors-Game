6629dbe688076296ffb647875a10f7af7f9d4c80
Ines <inespontes110@gmail.com>
2022-03-07 21:57:35 +0000

    Add images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const computerChoiceDisplay = document.getElementById(`computer-choice`);
 const userChoiceDisplay = document.getElementById(`user-choice`);
 const resultDisplay = document.getElementById(`result`);
+const img = document.querySelector(".ilustration");
 
 const possibleChoices = document.querySelectorAll("button");
 
